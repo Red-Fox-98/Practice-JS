c00f07ed22df512e780c94d1891266309bee2826
I.Susskiy <susskij14@gmail.com>
2024-02-20 22:07:14 +0700

    Переход на Vite

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     "plugin:prettier/recommended",
     "plugin:react-hooks/recommended"
   ],
-  plugins: ["react", "prettier"],
+  plugins: ["react", "prettier", 'react-refresh'],
   env: {
     browser: true,
     es6: true
@@ -36,10 +36,15 @@ module.exports = {
     "import/no-extraneous-dependencies": "off",
     "react/prop-types": "off",
     "react/jsx-filename-extension": "warn",
-    "react/react-in-jsx-scope": "warn",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
     "react/jsx-props-no-spreading": "warn",
     "jsx-a11y/no-static-element-interactions": "off",
     "jsx-a11y/click-events-have-key-events": "off",
-    "jsx-a11y/label-has-associated-control": "off"
+    "jsx-a11y/label-has-associated-control": "off",
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
   }
 };
