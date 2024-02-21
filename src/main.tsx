21440f7d51a98f890d4d6baa9a0ff7bd61634a56
Red-Fox-98 <evgeniya.zaitseva.98@gmail.com>
2024-02-21 12:50:07 +0300

    Правки

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app/App";
 import "./shared/styles/style.scss";
+import App from "./app/app.tsx";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
